ee17b0eaf7cdeb8a59d5fedea71923b61c60cc47
shao <shao.liang@breezecapsule.com>
2014-03-31 17:05:15 +0800

    Initial Commit

--- static/sys_root/shl8531@163.com/__sys.js
+++
@@ -1,3 +0,0 @@
-
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).setAttribute('src', '/sys_root/shl8531@163.com/app.js');
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).setAttribute('src', '/sys_root/shl8531@163.com/app2.js');
--- static/sys_root/th.synee@gmail.com/__sys.coffee
+++
@@ -1,1 +0,0 @@
- console.log("Hello Write") __sys.coffee __sys.coffee
